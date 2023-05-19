6f45e577f2131580a5f48bf89cfc891a0f68de23
Andre Campopiano <andre.campopiano@gmail.com>
2023-05-19 11:55:05 -0300

    Criação do projeto

---
+++ githubUserInformation/githubUserInformation/githubUserInformationApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubUserInformationApp.swift
+//  GitHubUserInformation
+//
+//  Created by Andre Luis Campopiano on 19/05/23.
+//
+
+import SwiftUI
+
+@main
+struct GitHubUserInformationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
