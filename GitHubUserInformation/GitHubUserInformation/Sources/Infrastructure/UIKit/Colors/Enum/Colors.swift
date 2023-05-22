b9289d49b6775805dc2265773d2fcde7a268a570
Andre Campopiano <andre.campopiano@gmail.com>
2023-05-22 11:25:28 -0300

    Implementation DS Colors

---
+++ GitHubUserInformation/GitHubUserInformation/Sources/Infrastructure/UIKit/Colors/Enum/Colors.swift
@@ -0,0 +1,42 @@
+//
+//  Colors.swift
+//  GitHubUserInformation
+//
+//  Created by Andre Luis Campopiano on 22/05/23.
+//
+
+import UIKit
+
+enum Colors: String {
+    
+    // MARK: - Neutral Colors
+    
+    case neutralDarkGrey
+    case neutralBlack
+    case neutralGrey
+    case neutralWhite
+    case neutralLightGrey
+    
+    // MARK: - Secoundary Colors
+    
+    case secondaryGreen
+    case secondaryRed
+    case secondaryDarkBlue
+    case secondaryDarkGreen
+    case secondaryLightGreen
+    case secondaryLightYellow
+    case secondaryNavy
+    case secondaryYellow
+    
+    // MARK: - Primary Colors
+    
+    case primaryBlue
+    case primaryArcticBlue
+    case primaryLightBlue
+    case primarySkyBlue
+    
+    public var color: UIColor {
+        UIColor(named: rawValue, in: .main, compatibleWith: .none) ?? .white
+    }
+    
+}
