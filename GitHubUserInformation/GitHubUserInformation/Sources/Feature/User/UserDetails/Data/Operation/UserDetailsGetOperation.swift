1dd6089395636e88996b137bc23bf8e8de11a6d2
Andre Campopiano <andre.campopiano@gmail.com>
2023-05-23 05:59:54 -0300

    [User Details Flow]
    create class UserDetailsManager
    create class UserDetailsGetOperation

---
+++ GitHubUserInformation/GitHubUserInformation/Sources/Feature/User/UserDetails/Data/Operation/UserDetailsGetOperation.swift
@@ -0,0 +1,46 @@
+//
+//  UserDetailsGetOperation.swift
+//  GitHubUserInformation
+//
+//  Created by Andre Luis Campopiano on 23/05/23.
+//
+
+import Foundation
+
+class UserDetailsGetOperation: NetworkingOperation {
+    
+    // MARK: - Constants
+    
+    private enum Constants {
+        static let urlPathFormat: String = "%@/%@"
+    }
+    
+    // MARK: - Properties
+    
+    var completion: UserDetailsGetCompletion
+    var business: UserDetailsBusinessProtocol?
+    var userName: String?
+    
+    // MARK: - Initialize
+    
+    required init(userName: String? = nil, provider: ApiProviderProtocol? = nil, business: UserDetailsBusinessProtocol? = nil, completion: @escaping UserDetailsGetCompletion) {
+        self.completion = completion
+        self.business = business
+        self.userName = userName
+        super.init(provider: provider)
+    }
+    
+    override func main() {
+        super.main()
+        provider?.baseRequest(requestData: setupNetworkingRequestData()) { [weak self] result in
+            guard let self = self else { return }
+            self.business?.handleUserDetailsGetCompletion(result: result, completion: self.completion)
+            self.finish()
+        }
+    }
+    
+    private func setupNetworkingRequestData() -> NetworkingRequestData {
+        let urlPath = String(format: Constants.urlPathFormat, ApplicationConstants.PathApi().user, userName ?? String())
+        return NetworkingRequestData(url: urlPath, httpMethod: .get)
+    }
+}
